fcb724c4fec3a8db8c19d1e8e32f3f5bb0d11b3d
scarlett-troncoso <scar.labussieri@gmail.com>
2024-04-08 21:00:35 +0200

    add while loop for database on html

--- index.php
+++ index.php
@@ -18,11 +18,61 @@ if ($connection && $connection->connect_error) {
 
 var_dump($connection);
 
-$sql = "SELECT * FROM `users`"; // seleziona tutti i post senza like
+$sql = "SELECT `posts`.`user_id`, COUNT(`likes`.`user_id`) AS `num_likes`
+FROM `posts`
+JOIN `likes` ON `likes`.`post_id` = `posts`.`id`
+GROUP BY `posts`.`user_id`
+ORDER BY `num_likes` DESC;"; // seleziona tutti i post senza like
 $result = $connection->query($sql); 
 
-var_dump($result); 
+// var_dump($result); 
+// var_dump($result->fetch_assoc());
+/*
+while ($row = $result -> fetch_assoc()) {
+    // var_dump($row); 
+    // var_dump($row['user_id']);
 
-var_dump($result->fetch_all()); 
+    ['user_id' => $user_id, 'num_likes' => $num_likes ] = $row; // destructuring
 
+    var_dump($user_id, $num_likes); // cosi mi fa vedere solo i campi ' ' e ' ' della prima riga della tabella departments
+
+    die;
+}*/
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Social Platform</title>
+    <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"/>
+</head>
+<body>
+    <main>
+        <div class="container py-4">
+            <table class="table">
+                <thead>
+                    <tr>
+                    <th scope="col">User Id</th>
+                    <th scope="col">Likes</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php while ($row = $result->fetch_assoc()) :
+                    ['user_id' => $user_id, 'num_likes' => $num_likes ] = $row; ?> 
+                        <tr>
+                            <th scope="row"><?= $user_id ?></th>
+                            <td><?= $num_likes ?></td>
+                        </tr>
+                    <?php endwhile; ?>
+                </tbody>
+            </table>           
+        </div>
+    </main>
+</body>
+</html>
