67467f5cffdf1787803a1b2e20d52810f0a55602
scarlett-troncoso <scar.labussieri@gmail.com>
2024-04-10 15:44:09 +0200

    add method on class media

--- Models/Media.php
+++ Models/Media.php
@@ -1,6 +1,6 @@
 <?php 
 /*
-● il sistema prevede immagini e video
+● il sistema prevede immagini e video 
 ● all'interno della classe sono dichiarate delle variabili d'istanza
 ● all'interno della classe è definito un costruttore
 ● all'interno della classe è definito almeno un metodo
@@ -8,14 +8,25 @@
 class Media {
     /*public function __construct(public int $id, public int $user_id, public string $type, public string $path, public string $created_at, public string $updated_at)
     {*/
-    public function __construct(/*public string $type,*/ public string $path)
+
+    public string $created_at;
+
+
+    public function __construct(public string $type, public array $path)
     {
         //$this->id = $id;
         //$this->user_id = $user_id;
-       // $this->type = $type;
+        $this->type = $type;
         $this->path = $path; //URL
         //$this->created_at = $created_at;
         //$this->updated_at = $updated_at;
+    }
+
+   
+    public function getInfoMedia(){
+        foreach ($this->path  as $pt) {
+            return $this->type . ' ' . 'link: ' . $pt;
+        }
     }
 }
 ?>
