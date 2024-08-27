9e55004f456b008eeee1b8a88fe5f3e7c8950d04
scarlett-troncoso <scar.labussieri@gmail.com>
2024-08-27 19:30:07 +0200

    change query SQL for data of posts

--- Models/PostDB.php
+++ Models/PostDB.php
@@ -9,44 +9,60 @@ require_once __DIR__ . '/Media.php';
 
 $connection = DB::connection_DB();
 
-$sql = "SELECT `media_post`.`post_id` AS `id_post`, `medias`.`id` AS `id_media`, `posts`.`title`, `posts`.`date`, `posts`.`tags`, `posts`.`created_at`, `medias`.`type` AS `type_media`, `medias`.`path` AS `path_media`
+/*$sql = "SELECT `media_post`.`post_id` AS `id_post`, `medias`.`id` AS `id_media`, `posts`.`title`, `posts`.`date`, `posts`.`tags`, `posts`.`created_at`, `medias`.`type` AS `type_media`, `medias`.`path` AS `path_media`
         FROM `medias`
         JOIN `media_post` ON `media_post`.`media_id` = `medias`.`id`
         JOIN `posts` ON `posts`.`id` = `media_post`.`post_id`;";
+$result_db = $connection->query($sql);*/
+
+$sql = "SELECT 
+            `posts`.`id` AS `id_post`, 
+            `posts`.`title`, 
+            `posts`.`date`, 
+            `posts`.`tags`, 
+            `posts`.`created_at`,
+            GROUP_CONCAT(`medias`.`type` ORDER BY `medias`.`id` ASC) AS `types_media`, 
+            GROUP_CONCAT(`medias`.`path` ORDER BY `medias`.`id` ASC) AS `paths_media`
+        FROM 
+            `posts`
+        JOIN 
+            `media_post` ON `posts`.`id` = `media_post`.`post_id`
+        JOIN 
+            `medias` ON `media_post`.`media_id` = `medias`.`id`
+        GROUP BY 
+            `posts`.`id`, `posts`.`title`, `posts`.`date`, `posts`.`tags`, `posts`.`created_at`
+        LIMIT 3;";
 $result_db = $connection->query($sql);
+
+// var_dump($result_db);
 
 DB::close_connection_DB($connection);
 
 
-/* While fino a 5 posts */
 
-$posts_db = [];
+/* While fino a 6 posts */
+
+/*$posts_db = [];
  
 $counter = 0;
-$max = 5;
+$max = 6;
 
 while (($res = $result_db -> fetch_assoc()) and ($counter < $max) ) {
     $counter++;
     array_push($posts_db, new Post($res['id_post'], $res['id_media'], $res['title'], $res['date'], $res['tags'], $res['created_at'], new Media($res['type_media'], $res['path_media'])) );
      
     //Ogni post si ripete la quantitá di volte dei media che ha, visto che ogni post ha piu di un media
+} */
+
+while ($res = $result_db->fetch_assoc()) {
+    $media_types = explode(',', $res['types_media']);
+    $media_paths = explode(',', $res['paths_media']);
+    
+    // Crea un nuovo Post e associa i media
+    $post = new Post($res['id_post'], $res['id_post'], $res['title'], $res['date'], $res['tags'], $res['created_at'], new Media(/*$type*/$media_types, $media_paths)/*$media_objects*/);
+
+    // Aggiungi il post alla lista dei post
+    $posts_db[] = $post;
+    //var_dump($post);
 }
-// abbiamo messo id_post al posto di id, e id_media al posto di user_id
-
-// var_dump($posts_db); // 5 risultati
-
-
-
-
-
-
-/* COSI SAREBBE STAMPARE IN PAGINA SENZA L'USO DELLA ISTANZA $posts_db
-<?php while (($res = $result_db -> fetch_assoc()) and ($i < 5)) :  $i++ ; 
-    ['id_post' => $id, 'id_media'=> $user_id, 'title'=> $title, 'date'=> $date, 'tags'=> $tags, 'created_at'=> $created_at, 'type_media'=> $type, 'path_media'=> $path] = $res ?> <!--stampare dal db senza utilizzare le istanze posts_db-->
-        <h4><?= $id?></h4>  
-        <h4><?= $title ?></h4>  
-        <h4><?= $type ?></h4>  
-        <h4><?= $path ?></h4>                                            
-<?php endwhile; ?>
-*/
 ?>
